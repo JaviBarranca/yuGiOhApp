a4bd32e51b180cb6959cebf4c230af6d0e639994
Javi Barranca <javierjbarranca@gmail.com>
2022-05-30 19:16:07 -0300

    Fix on routing

--- yuGiOhApp/src/app/app.module.ts
+++ yuGiOhApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http'
 
 import { CardModule } from './card/card.module';
